cbf8704c4e1a23a3840c62c30902c902e6f84bbd
grubhart <grubhart@gmail.com>
2025-11-02 21:45:01 -0600

    TESIS-119 feat(users): Implementar página de gestión de usuarios y refactorizar layout
    Excluidos los test del despliegue a produccion

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import { describe, test, expect, vi, beforeEach, afterEach } from 'vitest';
